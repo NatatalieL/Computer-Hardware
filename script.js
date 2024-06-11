de1c49814f12cd299b9b43b02f6c80f2dfb6de45
NatatalieL <167471297+NatatalieL@users.noreply.github.com>
2024-06-10 22:44:00 -0700

    FUCKYOU

---
+++ script.js
@@ -0,0 +1,24 @@
+function submitQuiz() {
+    // Collect user answers
+    let answers = [];
+    let correctAnswers = ["a", "c", "b", "a", "d", "true", "true", "true", "true"];
+    document.querySelectorAll('input[type="radio"]:checked').forEach(input => {
+        answers.push(input.value);
+    });
+    document.querySelectorAll('select').forEach(select => {
+        answers.push(select.value);
+    });
+
+
+    let score = 0;
+    for (let i = 0; i < answers.length; i++) {
+        if (answers[i] === correctAnswers[i]) {
+            score++;
+        }
+    }
+
+
+    // Calculate score (for demo, let's just display the answers)
+    alert("Answers: " + answers.join(", "));
+    alert("Your score: " + score + "/" + correctAnswers.length);
+}
